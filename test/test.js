f5ea5194eb51bef42b03402df9da5260c3e3eac7
Thomas Nilsen <thnilsen90@gmail.com>
2016-08-17 21:23:02 +0200

    added som unit tests.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,49 @@
-var results = require('../index');
+var chai = require('chai');
+var expect = chai.expect;
+var results = require('../index.js');
+var should = chai.should();
 
-results({
-    type: 'vikinglotto',
-}).then(function (response) {
-    console.log(response);
+
+
+it('should get the latest draw if no drawID is passed in', function (done) {
+    this.timeout(15000);
+
+    results({
+        type: 'vikinglotto',
+    }).then(function (response) {
+        done();
+    }).catch(function (err) {
+        done(err);
+    });
+});
+
+
+it('should list all results between two drawID\'s', function (done) {
+    this.timeout(15000);
+
+    results({
+        type: 'vikinglotto',
+        fromDrawID: 1050,
+        toDrawID: 1052
+    }).then(function (response) {
+        done();
+    }).catch(function (err) {
+        done(err);
+    });
+});
+
+
+it('should fail if no object is passed in', function () {
+    this.timeout(15000);
+
+    var fn = function () {
+        return results()
+            .then(function (response) {
+                
+            }).catch(function (err) {
+
+            });
+    };
+    
+    expect(fn).to.throw(Error);
 });
