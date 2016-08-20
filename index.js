147b39c08929c41537a23bcd8798e41f4f7333a0
Thomas Nilsen <thnilsen90@gmail.com>
2016-08-20 20:32:17 +0200

    Rewritten most of the logic in order to account for more scenarios

--- index.js
+++ index.js
@@ -5,21 +5,33 @@ module.exports = results;
 
 function toJSON(response) {
     var data = response.toString(),
-        matches = (data.match(/(^{[\s\w\W]+}$)/gm) || []).join(''),
+        matches = (data.match(/(^{[\s\w\W]+}$)/gm) || []).join(''),
         result = matches.length === 0 ? '[]' : matches;
 
     return JSON.parse(result);
 }
 
-function results(options) {
-    if (!options || !options.type) throw new Error('Expected option-object with key: type');
+function requestLatestdrawId(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, body) {
+            if (!error) {
+                var data = toJSON(body);
+                latestDrawID = data.drawID;
 
-    var endpoint = games[options.type] || games['vikinglotto'],
-        fromDrawID = options.fromDrawID || 0,
-        toDrawID = options.toDrawID || 0,
-        numberOfReqs = (toDrawID - fromDrawID) + 1;
+                resolve(latestDrawID);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
 
 
+function createPromises(numberOfReqs, fromDrawID, endpoint) {
+    if(typeof numberOfReqs !== 'number') {
+        throw new TypeError('Expected variable of type: Number');
+    }
+    
     var promises = Array(numberOfReqs)
         .fill(fromDrawID)
         .map(function (val, index) {
@@ -37,5 +49,45 @@ function results(options) {
             });
         });
 
-    return Promise.all(promises);
+    return promises;
+}
+
+function results(options) {
+    var endpoint,
+        fromDrawID = 0,
+        toDrawID = 0,
+        numberOfReqs = 1;
+
+    if (!options || !options.type) {
+        throw new Error('Expected option-object with key: type');
+    }
+
+    if (options.range && (!options.range.fromDrawID || !options.range.toDrawID)) {
+        throw new Error('If a range-object is present within the options-object, it must contains both fromDrawID and toDrawID properties');
+    }
+
+    endpoint = games[options.type] || games['vikinglotto'];
+
+
+    return requestLatestdrawId(endpoint)
+        .then(function (id) {
+
+            if (options.range) {
+                fromDrawID = options.range.fromDrawID;
+                toDrawID = options.range.toDrawID;
+                numberOfReqs = (toDrawID - fromDrawID) + 1;
+
+            } else if (options.fetchResult) {
+                var count = options.fetchResult
+                fromDrawID = (id - count) + 1;
+                numberOfReqs = count;
+            }
+
+            var promises = createPromises(numberOfReqs, fromDrawID, endpoint);
+            return Promise.all(promises);
+
+        })
+        .catch(function (err) {
+            console.log('Something went wrong... ' + err);
+        });
 }
